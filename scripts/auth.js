f65c8811e9cac96647f793a6b25c7cc2114ecc8d
mcorcias <mosescorcias@gmail.com>
2021-01-22 08:10:20 +0200

    Logout - auth.signout()

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,9 +9,18 @@ signupForm.addEventListener('submit', e => {
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
+    //console.log(cred.user);
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signes out');
   });
 });
