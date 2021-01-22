013802db30e7c7a949f8d06d89fab6fadbb244b7
mcorcias <mosescorcias@gmail.com>
2021-01-22 08:24:33 +0200

    Login - auth.signInWithEmailAndPassword

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,3 +24,21 @@ logout.addEventListener('click', e => {
     console.log('user signes out');
   });
 });
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Get the user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    // close the login modal and reset the form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
